540a3122b81cd8dda40fc71f7a029a6935f0c062
Chunxiao Li <lcx366@126.com>
2021-06-19 09:48:08 +0800

    trivial update

--- slrfield/utils/__init__.py
+++ slrfield/utils/__init__.py
@@ -7,5 +7,13 @@ This subpackage defines the following functions:
 
 tqdm_ftp - Try to download files by ftp.
 
-tqdm_request - Try to download files by request.
+tqdm_request - Try to download files by url.
+
+# data_download.py 
+
+download_eop - Download or update the Earth Orientation Parameters(EOP) file from IERS.
+
+download_ephem - Download JPL ephemeris files from NAIF.
+
+tle_download - Download the TLE/3LE data from https://www.space-track.org
 '''
