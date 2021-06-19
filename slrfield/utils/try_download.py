288082bd52ae9fa20a2ac19d3be6c5400a4fb84c
Chunxiao Li <lcx366@126.com>
2021-06-19 09:45:14 +0800

    Fixed the problem that ftp failed to download EOP

--- slrfield/utils/try_download.py
+++ slrfield/utils/try_download.py
@@ -4,46 +4,40 @@ from tqdm import tqdm
 from colorama import Fore
 from time import sleep
 
-def tqdm_ftp(ftp,dir_cpf_to,cpf_file,desc):
-    '''
-    Try to download files from remote server by ftp.
-    '''
-    total_size = int(ftp.size(cpf_file))
+def tqdm_ftp(ftp,dir_to,file,desc):
+    """
+    Try to download files from remote server by ftp with a colored progress bar.
+    """
+    dir_file = dir_to + file
+    total_size = int(ftp.size(file))
     bar_format = "{l_bar}%s{bar}%s{r_bar}" % (Fore.BLUE, Fore.RESET)
-    desc = 'Downloading {:s}'.format(cpf_file)
-    missing_flag = False
+
     for idownload in range(5):
         try:
-            local_file =  open(dir_cpf_to + cpf_file, 'ab')
+            local_file =  open(dir_file, 'wb')
             pos = local_file.tell()
             pbar = tqdm(desc = desc,total=total_size,unit='B',unit_scale=True,position=0,initial=pos,bar_format=bar_format)
             def progressbar(chunk):
                 local_file.write(chunk)
                 pbar.update(len(chunk))
-            res = ftp.retrbinary('RETR ' + cpf_file, progressbar, rest=pos) # RETR is an FTP command
+            res = ftp.retrbinary('RETR ' + file, progressbar) # RETR is an FTP command
             break       
         except:
-            sleep(3)
+            sleep(2)
             if idownload == 4:
-                remove(dir_cpf_to + cpf_file)
-                print('No response, skip {:s}'.format(cpf_file))
-                missing_flag = True
+                remove(dir_file)
+                print('No response, skip {:s}'.format(file))
         finally:
             pbar.close()
-            local_file.close()  
-            if missing_flag: 
-                return cpf_file  
-            else:
-                return None     
+            local_file.close()       
 
 
 def tqdm_request(url,dir_cpf_to,cpf_file,desc):
-    '''
-    Try to download files from remote server by request.
-    '''
+    """
+    Try to download files from url with a colored progress bar.
+    """
     block_size = 1024*10
     bar_format = "{l_bar}%s{bar}%s{r_bar}" % (Fore.BLUE, Fore.RESET)
-    desc = 'Downloading {:s}'.format(cpf_file)
     missing_flag = False
     for idownload in range(5):
         try:
