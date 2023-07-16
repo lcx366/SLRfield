c6ebbed02877527720fe5b3465abce3be20f0c8c
Chunxiao Li <lcx366@gmail.com>
2023-07-16 23:37:56 +0800

    Added downloading of Leap Second file from IERS and loading of external (non-buildin) EOP file)

--- slrfield/utils/try_download.py
+++ slrfield/utils/try_download.py
@@ -1,66 +1,19 @@
-from os import remove
-import requests
-from tqdm import tqdm
-from colorama import Fore
-from time import sleep
+import wget
 
-def tqdm_ftp(ftp,dir_to,file,desc):
+def wget_download(url,dir_file,desc=None):
     """
-    Try to download files from remote server by ftp with a colored progress bar.
-    """
-    dir_file = dir_to + file
-    total_size = int(ftp.size(file))
-    bar_format = "{l_bar}%s{bar}%s{r_bar}" % (Fore.BLUE, Fore.RESET)
+    download files by wget command
 
-    for idownload in range(5):
-        try:
-            local_file =  open(dir_file, 'wb')
-            pos = local_file.tell()
-            pbar = tqdm(desc = desc,total=total_size,unit='B',unit_scale=True,position=0,initial=pos,bar_format=bar_format)
-            def progressbar(chunk):
-                local_file.write(chunk)
-                pbar.update(len(chunk))
-            res = ftp.retrbinary('RETR ' + file, progressbar) # RETR is an FTP command
-            break       
-        except:
-            sleep(2)
-            if idownload == 4:
-                remove(dir_file)
-                print('No response, skip {:s}'.format(file))
-        finally:
-            pbar.close()
-            local_file.close()       
+    Inputs:
+        url -> [str] URL of the file to be downloaded
+        dir_file -> [str] path of the file to be downloaded
+        desc -> [str,optional] description of the downloading   
+    Outpits:
+        wget_out -> [str] path of the file downloaded
 
-def tqdm_request_cpf(url,dir_cpf_to,cpf_file,desc):
     """
-    Try to download cpf files from url with a colored progress bar.
-    """
-    block_size = 1024*10
-    missing_flag = False
-    bar_format = "{l_bar}%s{bar}%s{r_bar}" % (Fore.BLUE, Fore.RESET)
-    for idownload in range(5):
-        try:
-            local_file = open(dir_cpf_to + cpf_file, 'wb')
-            pos = local_file.tell()
-            res = requests.get(url,stream=True,timeout=200,headers={'Accept-Encoding': None,'Range': f'bytes={pos}-'})
-            total_size = int(res.headers.get('Content-Length'))
-            pbar = tqdm(desc = desc,total=total_size,unit='B',unit_scale=True,bar_format = bar_format,position=0,initial=pos)
-            for chunk in res.iter_content(block_size):
-                local_file.write(chunk)  
-                pbar.update(len(chunk))   
-            pbar.close()   
-            res.close()  
-            break
-        except: 
-            sleep(3)
-            if idownload == 4:
-                remove(dir_cpf_to + cpf_file)
-                print('No response, skip {:s}'.format(cpf_file)) 
-                missing_flag = True
-        finally:    
-            local_file.close() 
-            if missing_flag: 
-                return cpf_file  
-            else:
-                return None      
-                      
+    if desc: print(desc)
+    wget_out = wget.download(url,dir_file)
+    print()
+
+    return wget_out
